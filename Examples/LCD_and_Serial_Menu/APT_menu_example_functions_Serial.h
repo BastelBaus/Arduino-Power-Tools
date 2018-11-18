509bbb72a51b6ad35dbcdc801cc259ebf8b4aaad
BastelBaus <44684710+BastelBaus@users.noreply.github.com>
2018-11-19 00:28:49 +0100

    Corrections

--- Examples/LCD_and_Serial_Menu/APT_menu_example_functions_Serial.h
+++
@@ -1,5 +0,0 @@
-
-bool MENU3_callback(APT_Menu* menu, APT_MenuItem* entry, const uint8_t callbackType, const uint8_t line);
-bool MENU4_callback(APT_Menu* menu, APT_MenuItem* entry, const uint8_t callbackType, const uint8_t line);
-bool MENU5_callback(APT_Menu* menu, APT_MenuItem* entry, const uint8_t callbackType, const uint8_t line);
-bool MENU6_callback(APT_Menu* menu, APT_MenuItem* entry, const uint8_t callbackType, const uint8_t line);
